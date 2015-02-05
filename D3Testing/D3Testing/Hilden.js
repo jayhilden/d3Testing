de45821d00047ff7a27e7cf3bd42548ac56ff828
Jay Hilden <Jay.Hilden@verifycorporation.com>
2015-02-05 11:21:18 -0600

    successfully getting min/max values now

---
+++ D3Testing/D3Testing/Hilden.js
@@ -0,0 +1,29 @@
+﻿function coord(x, y) {
+    var c = [x, y];
+    return {
+        x: {
+            valueOf: function () {
+                var p = projection(c);
+                return p[0];
+            }
+        },
+        y: {
+            valueOf: function () {
+                var p = projection(c);
+                return p[1];
+            }
+        }
+    }
+}
+
+function initItemHilden(d) {
+    //debugger;
+    d.eventDate = Date.parse(d['Event Date']);
+    d.fromCoord = coord(d['From Latitude'], d['From Longitude']);
+    d.toCoord = coord(d['To Latitude'], d['To Longitude']);
+    d.movementDays = parseInt(d['Duration Of Movement In Days']);
+    //d.value = d.movementDays;//this is so we can do things like min/max calculations
+    //d.valueOf = d.value;//this is so we can do things like min/max calculations
+    d.daysAtDestination = parseInt(d['Days At Destination']);
+    return d;
+}
