91bc110ff8fa82f08d0a9fa0e456befec6d3f9c1
Jay Hilden <Jay.Hilden@verifycorporation.com>
2015-02-06 07:57:09 -0600

    getting first projection is working

---
+++ D3Testing/D3Testing/Scripts/D3Helpers.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+var projection = d3.geo.mercator();
+
+function Coordinate(x, y) {
+    var c = [x, y];
+    return {
+        x: {
+            valueOf: function () {
+                var p = projection(c);
+                return p[0];
+            }
+        },
+        y: {
+            valueOf: function () {
+                var p = projection(c);
+                return p[1];
+            }
+        },
+        projection: function () {
+            return projection(c);
+        }
+    }
+}
